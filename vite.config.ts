f50c416f6f1598642ee07d83527ee7466f0ce8f0
Jose Ornelas <joseornelaz@gmail.com>
2025-05-28 20:46:58 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/agc-react/', 
   resolve: {
     alias: {
       '@constants': path.resolve(__dirname, './src/types/index.tsx'),
