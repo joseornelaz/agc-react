374a1bcaf2bd9d17e6973cc20dc147e9bb6d38d2
Jose Ornelas <joseornelaz@gmail.com>
2025-05-28 20:53:59 -0700

    update router

--- src/AppRouting.tsx
+++ src/AppRouting.tsx
@@ -1,7 +1,7 @@
 import App from "./App";
 import { AppRoutingPaths } from "@constants";
-import { createBrowserRouter } from "react-router-dom";
-
+import { createHashRouter } from "react-router-dom";
+// createBrowserRouter
 import { 
     Calificaciones,
     Home, 
@@ -9,12 +9,12 @@ import {
     MiRuta
 } from "@components";
 
-export const AppRouting = createBrowserRouter([
+export const AppRouting = createHashRouter([
     {
         Component: App,
         children: [
             {
-                path: "/",
+                path: "/*",
                 Component: MainTemplate,
                 children: [
                     {
@@ -34,3 +34,28 @@ export const AppRouting = createBrowserRouter([
         ],
     }
 ]);
+// export const AppRouting = createBrowserRouter([
+//     {
+//         Component: App,
+//         children: [
+//             {
+//                 path: "/",
+//                 Component: MainTemplate,
+//                 children: [
+//                     {
+//                         path: AppRoutingPaths.BLANK,
+//                         Component: Home,
+//                     },
+//                     {
+//                         path: AppRoutingPaths.CALIFICACIONES,
+//                         Component: Calificaciones,
+//                     },
+//                     {
+//                         path: AppRoutingPaths.MIRUTA,
+//                         Component: MiRuta,
+//                     },
+//                 ]
+//             },
+//         ],
+//     }
+// ]);
