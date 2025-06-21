76db1880c41b620c59b1b31cd8dc118b6cef6eea
Jose Ornelas <joseornelaz@gmail.com>
2025-06-21 00:57:17 -0700

    deploy

--- src/components/pages/VideotecaBiblioteca/VideotecaBiblioteca.tsx
+++ src/components/pages/VideotecaBiblioteca/VideotecaBiblioteca.tsx
@@ -35,7 +35,7 @@ function TabPanel(props: TabPanelProps) {
 const VideotecaBiblioteca: React.FC = () => {
     const [value, setValue] = React.useState(0);
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
 
